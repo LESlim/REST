f9a27e6898117f3a4a644410a0594ecd636e4fdb
Stefan Lautner <stefanlautner@gmx.de>
2012-01-19 17:00:16 +0100

    add spring data JPA reference link

--- src/main/java/org/rest/persistence/dao/foo/IFooJpaDAO.java
+++ src/main/java/org/rest/persistence/dao/foo/IFooJpaDAO.java
@@ -3,6 +3,9 @@ package org.rest.persistence.dao.foo;
 import org.rest.model.Foo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * @see <a href="http://static.springsource.org/spring-data/data-jpa/docs/1.0.2.RELEASE/reference/html">spring-data-jpa</a>
+ */
 public interface IFooJpaDAO extends JpaRepository< Foo, Long >{
 	
 	Foo findByName( final String name );
